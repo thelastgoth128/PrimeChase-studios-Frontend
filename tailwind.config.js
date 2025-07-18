6ea2504c40a5dc4f614c41a7310692409f1e4967
thelastgoth128 <thindwacyrus86@gmail.com>
2025-07-18 08:56:02 +0200

    installed packages

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  content: ["./index.html", "./src/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        customBlue: "rgb(0, 115, 152)",
+      },
+    },
+  },
+  plugins: [],
+};
+a
