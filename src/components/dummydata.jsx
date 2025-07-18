317216cac51cd7305168eef0f2ba4bf24db995cc
thelastgoth128 <thindwacyrus86@gmail.com>
2025-07-18 08:56:44 +0200

    dummy data

---
+++ src/components/dummydata.jsx
@@ -0,0 +1,28 @@
+const products = [
+    {
+        id: "1",
+        image:"./jy.jpg",
+        cat: "",
+    },
+    {
+        id: "2",
+        image:"./jy.jpg",
+        cat:"",
+    },
+    {
+        id: "3",
+        image:"./vd.jpg",
+        cat: "",
+    },
+    {
+        id: "4",
+        image:"./story.jpg",
+        cat:"",
+    },
+    {
+        id: "5",
+        image:"./vd.jpg",
+        cat:"",
+    },
+]
+export default products;
