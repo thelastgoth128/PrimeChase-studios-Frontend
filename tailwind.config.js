f6e366a57a6b14cf0f6cec11cd24b60da6d629a6
thelastgoth128 <thindwacyrus86@gmail.com>
2025-07-22 11:10:30 +0200

    added transition css

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,21 @@ module.exports = {
       colors: {
         customBlue: "rgb(0, 115, 152)",
       },
+      keyframes: {
+        fadeInUp: {
+          '0%': {
+            opacity: '0',
+            transform: 'translate(30px)',
+          },
+          '100%': {
+            opacity: '1',
+            transform: 'translateY(0)',
+          },
+        },
+      },
+      animation: {
+        'fade-in-up': 'fadeInUp 2s ease-out forwards',
+      }
     },
   },
   plugins: [],
